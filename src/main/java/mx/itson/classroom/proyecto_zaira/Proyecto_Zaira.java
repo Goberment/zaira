969e317a802d5430d468b21adbd54fd781afea55
Goberment <Diego M@DESKTOP-JRHMECS>
2025-05-21 20:58:29 -0700

    Proyecto zaira

---
+++ src/main/java/mx/itson/classroom/proyecto_zaira/Proyecto_Zaira.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.classroom.proyecto_zaira;
+
+/**
+ *
+ * @author Diego M
+ */
+public class Proyecto_Zaira {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
